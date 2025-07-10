1ec77667a9931e4b55e93c05c0d738d6fdafccb7
samir1611 <johnshakya16@gmail.com>
2025-07-10 20:02:43 +0545

    useoptimization fixed

--- app/products-db/page.tsx
+++ app/products-db/page.tsx
@@ -1,7 +1,5 @@
 import { getProducts } from "@/prisma-db";
-import Link from "next/link";
-import { delProduct } from "../product-db-create/form-serveractions/product";
-import { useOptimistic } from "react";
+import ProductDetail from "./product-detail";
 
 type Product = {
   id: number;
@@ -12,40 +10,6 @@ type Product = {
 
 export default async function ProductDbpage() {
   const products: Product[] = await getProducts();
-  const [optimisticproducts, setOptimisticproducts] = useOptimistic(
-    products,
-    (currentProducts, deleteProductId) => {
-      return currentProducts.filter(
-        (product) => product.id !== deleteProductId
-      );
-    }
-  );
-  const removeProductById = async (productId: number) => {
-    setOptimisticproducts(productId);
-    await delProduct(productId);
-  };
-  return (
-    <ul className="space-y-4 p-4">
-      {optimisticproducts.map((product) => (
-        <li
-          key={product.id}
-          className="p-4 bg-white shadow-md rounded-lg text-gray-700"
-        >
-          <h2 className="text-lg font-semibold">
-            <Link href={`/products-db/${product.id}`}>{product.title}</Link>
-          </h2>
-          <p className="text-sm">{product.description}</p>
-          <p className="text-md font-bold mt-2">${product.price}</p>
-          <form action={removeProductById.bind(null, product.id)}>
-            <button
-              type="submit"
-              className="px-4 py-2 mt-4 text-white bg-red-500 hover:bg-red-600"
-            >
-              Delete
-            </button>
-          </form>
-        </li>
-      ))}
-    </ul>
-  );
+
+  return <ProductDetail products={products} />;
 }
